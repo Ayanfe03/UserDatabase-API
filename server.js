13a5823145dae3d80e132bc05cb79d3f4b5d7cbb
Olukemi Fasuan <ayanfe1711@gmail.com>
2024-09-21 00:15:34 +0100

    implementing CORS on server

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const userRouter = require('./routes/v1/users');
 const app = express();
 const PORT = 5500;
 
+const corsOptions = {
+  origin: 'http://greenmats.com',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+} // You must always implement CORS on your server when you're working with a server that's going to be working with a front-end
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/v1/users', userRouter);
 
